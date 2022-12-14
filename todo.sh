1cdeda702f3a4147ec376504bd75286a876a8746
Víctor Serrano Molina <victor.serrano.molina@estudiantat.upc.edu>
2022-12-14 17:53:11 +0100

    Todo.sh con cepstrum

--- todo.sh
+++ todo.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-NOM=trivial
+#eps=10
+for eps in $(seq 1.0 0.5 20.0); do #export LC_ALL=C
+
+numCoef=20
+
+NOM=cepstrum.$eps.$numCoef
 
 DIR_WORK=$PWD
 
@@ -59,10 +64,22 @@ EXEC_PRE=$DIR_PRM/$FUNCPRM.py
 [ -d $(dirname $EXEC_PRE) ] || mkdir -p $(dirname $EXEC_PRE)
 execPre="-x $EXEC_PRE"
 funcPrm="-f $FUNCPRM"
-eps=10
 echo "import numpy as np" | tee $EXEC_PRE
 echo "def $FUNCPRM(x):" | tee -a $EXEC_PRE
 echo "  return 10 * np.log10($eps + np.abs(np.fft.fft(x)) ** 2)" | tee -a $EXEC_PRE
+dirSen="-s $DIR_SEN"
+dirPrm="-p $DIR_PRM"
+
+FUNCPRM=cepstrum
+EXEC_PRE=$DIR_PRM/$FUNCPRM.py
+[ -d $(dirname $EXEC_PRE) ] || mkdir -p $(dirname $EXEC_PRE)
+execPre="-x $EXEC_PRE"
+funcPrm="-f $FUNCPRM"
+echo "import numpy as np" | tee $EXEC_PRE
+echo "def $FUNCPRM(x):" | tee -a $EXEC_PRE
+echo " logPdgm = 10 * np.log10($eps + np.abs(np.fft.fft(x)) ** 2)" | tee -a $EXEC_PRE
+echo " ceps = np.real(np.fft.ifft(logPdgm))" | tee -a $EXEC_PRE
+echo " return ceps[1:$numCoef + 1]" | tee -a $EXEC_PRE
 dirSen="-s $DIR_SEN"
 dirPrm="-p $DIR_PRM"
 
@@ -89,3 +106,5 @@ dirMar="-a $DIR_MAR"
 
 EXEC="evalua.py $dirRec $dirMar $GUI_REC"
 $EVA && echo $EXEC && $EXEC | tee $FIC_RES || exit 1
+
+done
